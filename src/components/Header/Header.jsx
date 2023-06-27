f254d31614eca83b2738c78c54062986f4ec88af
Jane Yushchenko <kelenella@ukr.net>
2023-06-27 15:15:04 +0300

    added Header markup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+
+import Button from "../Button/Button";
+import { ReactComponent as Logo } from "../../img/icons/Logo.svg";
+import s from "./Header.module.css";
+
+const Header = ({
+  navLinksObj: { textHome, textBlog, textAboutUs, textContactUs },
+}) => {
+  return (
+    <header className={s.header}>
+      <Logo />
+      <nav>
+        <ul className={s.navList}>
+          <li className={s.navItem}>
+            <a href="/" className={s.navLink}>
+              {textHome}
+            </a>
+          </li>
+          <li className={s.navItem}>
+            <a href="/" className={s.navLink}>
+              {textBlog}
+            </a>
+          </li>
+
+          <li className={s.navItem}>
+            <a href="/" className={s.navLink}>
+              {textAboutUs}
+            </a>
+          </li>
+
+          <li className={s.navItem}>
+            <a href="/" className={s.navLink}>
+              {textContactUs}
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Button text={"Subscribe"} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  navLinksObj: {
+    textHome: "Home",
+    textBlog: "Blog",
+    textAboutUs: "About Us",
+    textContactUs: "Contact Us",
+  },
+};
+
+Header.propTypes = {
+  navLinksObj: PropTypes.shape({
+    textHome: PropTypes.string,
+    textBlog: PropTypes.string,
+    textAboutUs: PropTypes.string,
+    textContactUs: PropTypes.string,
+  }),
+};
+
+export default Header;
