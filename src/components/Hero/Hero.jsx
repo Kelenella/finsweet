5865443a5bff401e4f1ab8607fba94912343a15a
Jane Yushchenko <kelenella@ukr.net>
2023-07-07 15:43:17 +0300

    added Hero markup

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,21 +4,23 @@ import s from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <>
-      <p>
+    <div className={s.hero}>
+      <p className={s.hero_topInfo}>
         Posted on <span>startup</span>
       </p>
-      <h1>Step-by-step guide to choosing great font pairs</h1>
-      <p>
-        By <span>James West</span> | May 23, 2022{" "}
+      <h1 className={s.titleHero}>
+        Step-by-step guide to choosing great font pairs
+      </h1>
+      <p className={s.hero_middleInfo}>
+        By <span>James West</span> | May 23, 2022
       </p>
-      <p>
+      <p className={s.hero_bottomInfo}>
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
         dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident.
       </p>
       <Button text={"Read More >"} type={Button} />
-    </>
+    </div>
   );
 };
 
