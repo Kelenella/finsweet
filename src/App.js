7248cd6d6eaabbd2ae4f5d7f8f9f26bc4d3d61cd
Jane Yushchenko <kelenella@ukr.net>
2023-07-14 15:41:48 +0300

    created router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import ChooseCategory from "./components/ChooseCategory/ChooseCategory";
-import ListOfAuthors from "./components/ListOfAuthors/ListOfAuthors";
-import JoinOurTeam from "./components/JoinOurTeam/JoinOurTeam";
-import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+// import Header from "./components/Header/Header";
+// import Hero from "./components/Hero/Hero";
+// import ChooseCategory from "./components/ChooseCategory/ChooseCategory";
+// import ListOfAuthors from "./components/ListOfAuthors/ListOfAuthors";
+// import JoinOurTeam from "./components/JoinOurTeam/JoinOurTeam";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <ChooseCategory />
-      <ListOfAuthors />
-      <JoinOurTeam />
-      <Footer />
+      <HomePage />
     </>
   );
 }
